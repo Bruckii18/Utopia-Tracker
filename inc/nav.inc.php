ded118eeeaa9047fac0af2dbd3761bf1a92afe4b
Noran <matschiff@gmx.de>
2020-10-13 15:01:31 +0200

    navbar

---
+++ inc/nav.inc.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><i class="fas fa-map-marked-alt"></i> Utopia-Tracker</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="utopiaNav" aria-expanded="false" area-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="utopiaNav">
+        <div class="col-12 text-center">
+            <ul class="navbar-nav">
+
+                <li class="col nav-item">
+                    <a class="nav-link" href="#">Übersicht</a>
+                </li>
+                <li class="col nav-item">
+                    <a class="nav-link" href="#">Liste</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
